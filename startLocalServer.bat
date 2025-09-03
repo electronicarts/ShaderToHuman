33191983cbf091b2541e200e1df3e52f38c4fe74
Martin Mittring <mmittring@ea.com>
2025-09-02 18:57:41 -0700

    Fixed missing "node install express" when running startLocalServer.bat

--- startLocalServer.bat
+++ startLocalServer.bat
@@ -1,3 +1,12 @@
-rem node server.js
-rem npm install
+@echo off
+rem run from window with double click or from cmd.exe or Powershell
+
+rem the following line tests if express is installed and valid (tree is not empty)
+node -e "try{require('express');process.exit(0);}catch(e){process.exit(1);}"
+if errorlevel 1 (
+    rem for convenience, install express locally if not already installed
+    echo "installing express (see folder node_modules) ..."
+    call npm install express
+)
+
 npm start
